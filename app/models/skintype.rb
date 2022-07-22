048a4a7ab006f0b32ebcdaeb3f4664f7ba3298c0
dongwoo1122 <yoshikigota@gmail.com>
2022-07-22 13:13:52 +0900

    カテゴリー・画像投稿機能の導入

---
+++ app/models/skintype.rb
@@ -0,0 +1,14 @@
+class Genre < ActiveHash::Base
+  self.data = [
+    { id: 1, name: '---' },
+    { id: 2, name: '乾燥肌' },
+    { id: 3, name: '脂性肌（オイリー肌）' },
+    { id: 4, name: '混合肌（インナードライ）' },
+    { id: 5, name: '普通肌' },
+    { id: 6, name: '敏感肌' }
+  ]
+
+  include ActiveHash::Associations
+  has_many :users
+
+  end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,14 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :routineposts
+  has_many :logposts
+  has_one_attached :image
+
+  validates :nickname, presence: true
+  validates :skintype_id, numericality: { other_than: 1 } 
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :skintype
+  
 end
